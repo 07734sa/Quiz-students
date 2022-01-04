805408f6ebbe4014b125bd36cd6d3b0c61565692
50773sa <93046590+50773sa@users.noreply.github.com>
2022-01-04 17:05:07 +0100

    give it another try

--- script_2.js
+++ script_2.js
@@ -1,19 +1,3 @@
-/*
-Godkänt
-
-- Följer strukturen och upplägget ovan
-- Den ursprungliga array:en förblir oförändrad
-- Visar resultat med antal rätt samt totalt antal frågor (t.ex. 16/20 rätt om man fick 16 rätt på 20 frågor)
-
-Väl Godkänt
-
-- Använda Array Filter & Array Map
-- Slumpa ordningen som bilderna kommer i
-- Visa vilka man gissade fel på och vilket som var rätt svar. Antingen genom att i listan visa vad man gissade på 
-    och vad som var rätt svar, eller i en ny lista under resultatet visa foto + namn på de man gissade fel på
-- Håll reda på senaste resultat och vid varje ny gissning visa om man förbättrade eller försämrade sig denna gången
-*/
-
 const photoEl = document.querySelector('.photo');
 const guessBtnWrap = document.querySelector('.guessBtnWrap');
 const lightboxEl = document.querySelector('#lightboxWrapper')
@@ -183,10 +167,13 @@ const students = [
 ];
 //console.log(students);
 
-let correctGuessIndex = ""; // Måste ligga utanför. getStudents kan inte komma åt den inne i scopet
 let names;
+let correctGuessIndex = "";
+let correctAnswer = [];  
+let incorrectAnswer = [];  
+let removeShowedObj = [];
 
-let guesses = 0; 
+let guesses = 0;
 let correctPoints = 0;
 
 //----------SHUFFLE STUDENTS-------------------------------------------------
@@ -201,7 +188,7 @@ const shuffleArray = (array) => { //shuffle names in copied array
     }
 };
 
-//-----------SHOW  ONE IMAGE AND 4 BUTTON WITH NAMES-------------------------------------------
+//-----------SHOW ONE IMAGE AND 4 BUTTONS WITH NAMES-------------------------------------------
 
 const showStudent = () => { // skapar en funktion för att kunna kalla på den igen
 	guessBtnWrap.innerHTML = ""; //Gör att fler knappar ej skapas.
@@ -218,81 +205,80 @@ const showStudent = () => { // skapar en funktion för att kunna kalla på den i
 
 	names.forEach(name => { //För varje namn skapas en knapp.
 		guessBtnWrap.innerHTML += `<button class="guess">${name}</button>`;
-	});
-
+	}); 
 };
 
-//-----------ADD CLICK EVENTS FOR EACH NAME-----------------------------------
+//------------START GAME------------------------------------------------------
 
-showStudent();
 
-guessBtnWrap.addEventListener('click', e  => {
-	e.preventDefault();
 
-	if ('BUTTON' === e.target.tagName && guesses < 10){ //Om target är en button... Här ska 10 foton visas
-		guesses ++; //Antal gissade ggr
-		console.log(e.target);
-	
+guessBtnWrap.addEventListener('click', e => {
+    e.preventDefault();
+  
+    if (e.target.tagName === 'BUTTON') {
+        guesses++;
+    
+        if (e.target.innerText === correctGuessIndex.name) {
+            correctPoints++;
 
-	if (e.target.innerText === correctGuessIndex.name) {	 //...om namnet på knappen hör ihop med bilden.
-		correctPoints++ //  Om det är rätt, 1 poäng
-		e.target.classList.add('correct')
-		
-	}	else if (guesses === 10) { //När spelet har visat 10 bilder visas lightboxen...
-			lightboxEl.classList.add('show');
-			resultButtonEl.classList.add('show');//...och resultatknappen
+            correctAnswer.push(correctGuessIndex)
+            removeShowedObj.pop(correctGuessIndex)
+            console.log(correctAnswer);
 
-	}	else {
-			e.target.classList.add('wrong')
-	}
-	
-}
+        }  else {
+            incorrectAnswer.push(correctGuessIndex)
+            removeShowedObj.pop(correctGuessIndex)
+
+            console.log(incorrectAnswer)
+
+        }
+
+        if (guesses === 10) {
+            lightboxEl.classList.add('show');
+            resultButtonEl.classList.add('show');//...och resultatknappen
+        }
+    }
+    showStudent();
 });
 
-//----------- HIDE RESULT BUTTON WHEN CLICKED ON. THEN SHOW SCORE--------------
+
+//----------------RESULT BUTTON-------------------------------------------
 
 resultButtonEl.addEventListener('click', e => {
 	e.preventDefault();
 	console.log(e.target)
 
-	/*
-	//Filter student
-	userGuesses.filter(student => student.userGuess === false) //filtrera ut fel svar
-	userGuesses.forEach(guess  => { // för varje fel svar, visa bild och
-		wrongStudentsEl.innerHTML += `
-		<figure>
-			<img src="students/${guess.image}" alt="wrongstudent">
-			<figcaption>${guess.name}</figcaption>
-		</figure>
-		`	
-	});
-	*/
-	
+
 	if ('BUTTON' === e.target.tagName) {
-			
+
 		resultButtonEl.classList.remove('show'); // resultatknappen döljs
 		resultEl.classList.add('show');
-		resultEl.innerText =`Antal rätt: ${correctPoints}/${guesses}`; //poäng
+		resultEl.innerText = `Antal rätt: ${correctPoints}/${guesses}`; //poäng
 		playAgainButtonEl.classList.add('show');
 	}	
 });
+
 
 //------------------PLAY AGAIN BUTTON--------------------------------------
 
 playAgainButtonEl.addEventListener('click', e => {
 
 	if ('BUTTON' === e.target.tagName) {
+        //Rensar arrays
+        correctAnswer = [];  
+        incorrectAnswer = [];  
+        removeShowedObj = [];
+    
 		guesses = 0; 	//När spelet är slut nollställs räknaren
 		correctPoints = 0;
 
 		resultEl.classList.remove('show');	//Dölj resultat
 		playAgainButtonEl.classList.remove('show'); //Dölj splela-igen-knappen
 		lightboxEl.classList.remove('show'); // Dölj lightbox
-	}				
-	showStudent();
+	}	
+    showStudent(); //annars visar den smma person om man väljer att spela igen.			
 });
 
 showStudent();
 
-//-------------------------------------------------------------------------------
 
